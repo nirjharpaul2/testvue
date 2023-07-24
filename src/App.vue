034e991914b7143e25f999e6ff060de16f181361
nirjharkpaul <nirjhar.paul@tesco.com>
2023-07-24 17:14:18 +0530

    updated code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <AppNavBar />
     <router-view />
   </div>
 </template>
-
+<script>
+import AppNavBar from "@/components/AppNavBar.vue";
+export default {
+  name: "AppVue",
+  components: {
+    AppNavBar,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,123 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { ref } from "vue";
+
+const functions = ref([
+  { id: 0, name: "shirt" },
+  { id: 1, name: "jacket" },
+  { id: 2, name: "shoes" },
+  { id: 3, name: "shoes" },
+  { id: 4, name: "shoes" },
+  { id: 5, name: "shoes" },
+]);
+
+const mytasks = ref([
+  { id: 0, name: "shirt" },
+  { id: 1, name: "jacket" },
+  { id: 2, name: "shoes" },
+]);
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      functions,
+      mytasks,
+    };
   },
 };
 </script>
+<style scoped lang="scss">
+.card-wrapper-div {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  padding: 5px 20px;
+}
+</style>
+
+<template>
+  <div class="home">
+    <div>
+      <div>
+        <b-tabs content-class="mt-3" fill>
+          <b-tab title="First" active>
+            <div class="card-wrapper-div">
+              <div
+                v-for="(carddetails, index) in functions"
+                :key="carddetails.id"
+                ref="{{functions}}"
+              >
+                <div class="cardwrapper">
+                  <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem"
+                    class="mb-2"
+                  >
+                    <template #header>
+                      <h4 class="mb-0">Hello World</h4>
+                    </template>
+                    <b-card-body>
+                      <b-card-title>Card Title</b-card-title>
+                      <b-card-sub-title class="mb-2"
+                        >Card Sub Title</b-card-sub-title
+                      >
+                      <b-card-text>
+                        {{ functions.name }} -- {{ index }}
+                      </b-card-text>
+                      <b-button href="#" variant="primary"
+                        >Go somewhere</b-button
+                      >
+                    </b-card-body>
+                    <b-card-footer>This is a footer</b-card-footer>
+                  </b-card>
+                </div>
+              </div>
+            </div>
+          </b-tab>
+          <b-tab title="Second">
+            <div class="card-wrapper-div">
+              <div
+                v-for="(carddetails, index) in mytasks"
+                :key="carddetails.id"
+                ref="{{mytasks}}"
+              >
+                <div class="cardwrapper">
+                  <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem"
+                    class="mb-2"
+                  >
+                    <template #header>
+                      <h4 class="mb-0">Hello World</h4>
+                    </template>
+                    <b-card-body>
+                      <b-card-title>Card Title</b-card-title>
+                      <b-card-sub-title class="mb-2"
+                        >Card Sub Title</b-card-sub-title
+                      >
+                      <b-card-text>
+                        {{ functions.name }} -- {{ index }}
+                      </b-card-text>
+                      <b-button href="#" variant="primary"
+                        >Go somewhere</b-button
+                      >
+                    </b-card-body>
+                    <b-card-footer>This is a footer</b-card-footer>
+                  </b-card>
+                </div>
+              </div>
+            </div>
+          </b-tab>
+        </b-tabs>
+      </div>
+    </div>
+  </div>
+</template>
